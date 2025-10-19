b5b9514a918510e07819a5ab96b0d87f90160403
atom-dispencer <46679475+atom-dispencer@users.noreply.github.com>
2025-10-19 18:11:57 +0100

    Now we're looking from above!

--- src/glsl/vertex.glsl
+++ src/glsl/vertex.glsl
@@ -14,6 +14,20 @@ mat4 translate(vec3 v)
     );
 }
 
+mat4 x_rotation(float rads)
+{
+    float c = cos(rads);
+    float s = sin(rads);
+
+    // For some reason glsl matrices are column-major
+    return mat4(
+        1.0,  0.0,  0.0,  0.0, //
+        0.0,  c,    s,    0.0, //
+        0.0,  -s,    c,   0.0, //
+        0.0,  0.0,  0.0,  1.0 //
+    );
+}
+
 mat4 y_rotation(float rads)
 {
     float c = cos(rads);
@@ -32,8 +46,12 @@ void main()
 {
   vColor = normalize(aPos * 0.5 + 0.5);
 
-  // Transformations applied in reverse order:
-  // - Rotate the mesh
-  // - Translate away from the camera
-  gl_Position = translate(vec3(0.0, 0.0, 0.2)) * y_rotation(y_rads) * vec4(aPos.x, aPos.y, aPos.z, 1.0);
+  float PITCH_RADS = 3.14159 * -0.25;
+
+  gl_Position = //
+    translate(vec3(0.0, 0.0, 0.2))  // 3rd - Translate away from the camera
+    * x_rotation(PITCH_RADS)        // 2nd - Pitch the mesh like we're looking from above
+    * y_rotation(y_rads)            // 1st - Yaw the mesh so it spins nicely
+    // 
+    * vec4(aPos.x, aPos.y, aPos.z, 1.0);
 }
