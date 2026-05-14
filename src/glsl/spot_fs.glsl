49163290aa7fda1f50b1993b6db43f02297952c7
Adam <46679475+atom-dispencer@users.noreply.github.com>
2026-05-14 17:57:02 +0100

    Dimmer spotlight

--- src/glsl/spot_fs.glsl
+++ src/glsl/spot_fs.glsl
@@ -8,5 +8,13 @@ out vec4 FragColor;
 
 void main()
 {
-  FragColor = texture(spot_texture, TexCoord);
+  vec4 texture_colour = texture(spot_texture, TexCoord);
+
+  float brightness = 0.6;
+  FragColor = vec4(
+    texture_colour.x * brightness,
+    texture_colour.y * brightness,
+    texture_colour.z * brightness,
+    texture_colour.w
+  );
 }
